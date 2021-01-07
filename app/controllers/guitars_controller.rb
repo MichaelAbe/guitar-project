9238db7a164f766e3c4d0dfcaba7acf562708e1e
MichaelAbe <michael.k.abe@gmail.com>
2021-01-07 09:32:13 -0500

    added delete button/functionality

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -10,11 +10,13 @@ class GuitarsController < ApplicationController
     
     get '/guitars/:id/edit' do
         @guitar = Guitar.find_by_id(params[:id])
+        redirect '/guitars' unless @guitar
         erb :'guitars/edit'
     end
     
     get '/guitars/:id' do
         @guitar = Guitar.find_by_id(params[:id])
+        redirect '/guitars' unless @guitar
         #binding.pry
         erb :'guitars/show'
     end
@@ -32,14 +34,21 @@ class GuitarsController < ApplicationController
     patch '/guitars/:id' do
         #binding.pry
         @guitar = Guitar.find_by_id(params[:id])
-
-    if @guitar.update(params[:guitar])
-      redirect "/guitars/#{@guitar.id}"
-    else
-      redirect "/guitars/#{@guitar.id}/edit"
+        redirect '/guitars' unless @guitar
+        if @guitar.update(params[:guitar])
+            redirect "/guitars/#{@guitar.id}"
+        else
+            redirect "/guitars/#{@guitar.id}/edit"
+        end
     end
-    end
 
+    delete '/guitars/:id' do
+        @guitar = Guitar.find_by_id(params[:id])
+        if @guitar
+            @guitar.destroy
+            redirect '/guitars'
+        end
+      end
     # patch '/guitars/:id/' do
     #     "Hello World"
     #   end
