12f140d0c2546e9a8328574d53521ebd737ad7f8
MichaelAbe <michael.k.abe@gmail.com>
2021-01-08 13:52:17 -0500

    setup login beginning

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,11 +11,27 @@ class SessionsController < ApplicationController
             redirect "/guitars"
         else
             redirect "/signup"
-        binding.pry
+        #binding.pry
+        end
+    end
+    
+    get '/login' do     
+        erb :'sessions/login'
+    end
+
+    post '/login' do
+        #binding.pry
+        user = User.find_by_username(params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect '/guitars'
+        else
+            redirect '/login'
         end
     end
 
     get '/logout' do
-        "Hello World"
+        session.clear
+        redirect '/login'
     end
 end
