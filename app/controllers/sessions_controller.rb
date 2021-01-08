651109102ca213deaf9120ad594ab3475a5c1120
MichaelAbe <michael.k.abe@gmail.com>
2021-01-08 13:05:31 -0500

    working on the login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+    get '/signup' do
+        
+    erb :'sessions/signup'
+    end
+
+    post '/signup' do 
+        user = User.new(params[:user])
+        if user.save
+            session[:user_id] = user.id
+            redirect "/guitars"
+        else
+            redirect "/signup"
+        binding.pry
+        end
+    end
+end
